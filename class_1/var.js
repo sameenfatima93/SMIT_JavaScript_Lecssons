fa023d5db251066e4a1df4ab27b365908f11c1ae
SameenFatima <sameen.fatima993@gmail.com>
2025-07-24 07:31:07 +0500

    task_6_to_8

--- class_1/var.js
+++ class_1/var.js
@@ -1,5 +1,22 @@
-var userName = "aarish Daniyal  ";
-var myName = " Sameen Fatima "
-var massage = " my nepheuw "
-var fullMassege = userName + myName + massage;
-alert(fullMassege)
+// var userName = "aarish Daniyal  ";
+// var myName = " Sameen Fatima "
+// var massage = " my nepheuw "
+// var fullMassege = userName + myName + massage;
+// alert(fullMassege)
+
+//********* task_5 ****************//
+// var foodIteme = "pizza \n pizz \n piz \n pi \n p"
+// alert(foodIteme)
+
+
+//********task_6***********//
+// var email = "my email address is"
+// var myEmailAdd = email + "example@google.com"
+// alert(myEmailAdd)
+
+//*******task_7***********//
+// var book = "a smater way to learn javascript"
+// alert("i am trying to learn from the book" + book)
+
+//*******task_8*********//
+// document.write("<h1> yah! i can write a HTML code in thorugh the javascript </h1>")
